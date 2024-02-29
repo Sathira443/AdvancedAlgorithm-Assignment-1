1e8e14ee064c47e77e4756ab2cdc695adbd3c51d
Sathira Liyanapathirana <liyanapathiranasathira@gmail.com>
2024-02-29 15:15:36 +0530

    Comma Issue corrected

--- test.cpp
+++ test.cpp
@@ -11,104 +11,49 @@
 using namespace std;
 using namespace chrono;
 
-void insert(string combined_string)
-{
-    
-}
-
 int main()
 {
     std::vector<std::string> sets = {"set1", "set2"};
     std::vector<std::string> txt_files = {"data_1.txt", "data_2.txt", "data_3.txt"};
 
-    for (const std::string &set : sets)
+    BST bst;
+
+    ifstream data_file("data/data/insert/set1/data_1.txt");
+
+    if (!data_file.is_open())
     {
-        for (const std::string &txt_file : txt_files)
+        cerr << "Error: Could not open data file." << endl;
+        return 1;
+    }
+
+    string line;
+
+    auto start_bst = high_resolution_clock::now();
+    while (getline(data_file, line))
+    {
+        stringstream next_line(line);
+        string number_str;
+
+        while (getline(next_line, number_str, ','))
         {
-            std::string combined_string = "data/data/insert/" + set + "/" + txt_file;
-            std::cout << combined_string << std::endl;
-
-            BST bst;
-            RBTree rbtree;
-            SplayTree splaytree;
-
-            ifstream data_file(combined_string);
-
-            if (!data_file.is_open())
+            if (!next_line.eof())
             {
-                cerr << "Error: Could not open data file." << endl;
-                return 1;
+                long long int number = stoll(number_str);
+                // cout << "Number is: " << number << " number" << endl;
+                bst.put(number, number);
             }
 
-            string line;
-
-            auto start_bst = high_resolution_clock::now();
-            while (getline(data_file, line))
-            {
-                stringstream ss(line);
-
-                int number;
-                while (ss >> number)
-                {
-                    bst.put(number, number);
-                }
-            }
-            auto stop_bst = high_resolution_clock::now();
-            auto duration_bst = duration_cast<microseconds>(stop_bst - start_bst);
-
-            data_file.close();
-
-            data_file.open(combined_string);
-            if (!data_file.is_open())
-            {
-                cerr << "Error: Could not open data file." << endl;
-                return 1;
-            }
-
-            auto start_rbtree = high_resolution_clock::now();
-            while (getline(data_file, line))
-            {
-                stringstream ss(line);
-
-                int number;
-                while (ss >> number)
-                {
-                    rbtree.put(number, number);
-                }
-            }
-            auto stop_rbtree = high_resolution_clock::now();
-            auto duration_rbtree = duration_cast<microseconds>(stop_rbtree - start_rbtree);
-
-            data_file.close();
-
-            data_file.open(combined_string);
-            if (!data_file.is_open())
-            {
-                cerr << "Error: Could not open data file." << endl;
-                return 1;
-            }
-
-            auto start_splaytree = high_resolution_clock::now();
-            while (getline(data_file, line))
-            {
-                stringstream ss(line);
-
-                int number;
-                while (ss >> number)
-                {
-                    splaytree.put(number, number);
-                }
-            }
-            auto stop_splaytree = high_resolution_clock::now();
-            auto duration_splaytree = duration_cast<microseconds>(stop_splaytree - start_splaytree);
-
-            data_file.close();
-
-            cout << "Total BST insertion time: " << duration_bst.count() << " microseconds" << endl;
-            cout << "Total RBTree insertion time: " << duration_rbtree.count() << " microseconds" << endl;
-            cout << "Total SplayTree insertion time: " << duration_splaytree.count() << " microseconds" << endl;
+            
         }
     }
+    auto stop_bst = high_resolution_clock::now();
+    auto duration_bst = duration_cast<microseconds>(stop_bst - start_bst);
+
+    data_file.close();
+
+    bst.print_in_order();
+
+    cout << "Total BST insertion time: " << duration_bst.count() << " microseconds" << endl;
 
     return 0;
 }
